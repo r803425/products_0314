4836c5f4519bb7b7c461a4ef525b6323d75c274e
Ryan <r803425@gmail.com>
2023-03-14 19:47:55 +0800

    add check file code

--- products.py
+++ products.py
@@ -1,17 +1,36 @@
 # 記帳程式
-# 5.讀取檔案
+# 6.檢查檔案，如果沒有檔案會出錯
+import os # 作業系統，operating system
+
 products = []
-with open('products.csv', 'r', encoding = 'utf-8') as f:
-    for line in f:
-        if '商品,價格' in line:
-            continue # 強制跳下一迴圈，並不是逃出迴圈
-        name, price = line.strip().split(',')
-        products.append([name, price])
-print(products)
-print('---------------------------------')
+if os.path.isfile('products.csv'): # 相對路徑，檢查檔案在不在
+    print('成功找到檔案')
+    with open('products.csv', 'r', encoding = 'utf-8') as f:
+        for line in f:
+            if '商品,價格' in line:
+                continue # 強制跳下一迴圈，並不是逃出迴圈
+            name, price = line.strip().split(',')
+            products.append([name, price])
+    print(products)
+    print('---------------------------------')
+else:
+    print('找不到檔案哦!!!，請寫入檔案新增')
+    print('---------------------------------')
+
+# 5.讀取檔案
+# 變換到第6步驟裡面
+# products = []
+# with open('products.csv', 'r', encoding = 'utf-8') as f:
+#     for line in f:
+#         if '商品,價格' in line:
+#             continue # 強制跳下一迴圈，並不是逃出迴圈
+#         name, price = line.strip().split(',')
+#         products.append([name, price])
+# print(products)
+# print('---------------------------------')
 
 # １.輸入商品名稱(輸入q離開)／價格， 並存入列表
-products = []
+# products = []
 while True:
     name = input('請輸入名稱(按q/Q離開) : ')
     if name == 'q' or name == 'Q':
